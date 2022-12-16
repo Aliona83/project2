84b9b86d683bfd713eb85a42f24d36f5552bc5b6
Aliona83 <alionajanusko@gmail.com>
2022-12-16 12:57:36 +0000

    Get quiz timer and question count working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,129 +1,127 @@
 // The arrays of quiz question
-const quizData = [
-    {
-      question: "Where did the Christmas tree originate from?",
-      a: "Germany",
-      b: "Ireland",
-      c: "Norway",
-      d: "Sweden",
-      correct: "a",
-    },
-    {
-      question: "Who invented electric lights for the Christmas tree?",
-      a: "Ernest Hemingway",
-      b: "Saul Bellow",
-      c: "Edward H. Johnson",
-      d: "Frank Kafka",
-      correct: "c",
-    },
-    {
-      question: "Where is the oldest Christmas market in the world?",
-      a: " Cologne Germany",
-      b: "Dresden Striezelmarkt",
-      c: "Birmingham Christmas Market",
-      d: "Seville's Christmas Market Spain",
-      correct: "b",
-    },
-    {
-      question:"What is the name for the period in between Christmas and New Year?",
-      a: "Turxmas",
-      b: "Landxmas",
-      c: "Twelvetide",
-      d: "Twixmas",
-      correct: "d",
-    },
-    {
-      question: "What is the most popular Christmas tree topper ornament?",
-      a: "An angel",
-      b: "Snowflake",
-      c: "Star",
-      d: "Elf",
-      correct: "a",
-    },
-    {
-      question: "What country started the tradition of hanging stockings?",
-      a: "Canada",
-      b: "Australia",
-      c: "Italy",
-      d: "Germany",
-      correct: "c",
-    },
-    {
-      question: "Which popular christmas beverage is also called 'milk punch?",
-      a: "Milkpunch",
-      b: "Eggnog",
-      c: "Milkbag",
-      d: "Eggpunch",
-      correct: "b",
-    },
-    {
-      question: "What plant is sometimes known as the 'Christmas flower?",
-      a: "Bromeliad",
-      b: "amarillis",
-      c: "Shamrock",
-      d: "Poinsettias",
-      correct: "d",
-    },
-    {
-      question:"In which country is it tradition to eat KFC for Christmas dinner?",
-      a: "Japan",
-      b: "Germany",
-      c: "Ireland",
-      d: "China",
-      correct: "a",
-    },
-     {
-      question: "1.	What is the name of the ballet that takes place at Christmas?",
-      a: "The Sleeping Beauty",
-      b: "The Nutcracker",
-      c: "The Little Match Girl",
-      d: "The Snowman",
-      correct: "b",
-     },
-     {
-      question: "What is traditionally hidden inside a Christmas pudding?",
-      a: "Button",
-      b: "Pin",
-      c: "A coin",
-      d: "Stone",
-      correct: "c",
-     },
-     {
-      question: "If you are born on Christmas day, what is your star sign?",
-      a: "Taurus",
-      b: "Capricorn",
-      c: "Virgo",
-      d: "Aries",
-      correct: "b",
-     },
-     {
-      question:
-        "According to tradition, when should you have your Christmas decorations down by?",
-      a: "January 5th",
-      b: "January 2nd",
-      c: " January 7th",
-      d: "January 14yh",
-      correct: "a",
-     },
-     {
-      question: "What decoration do elves traditionally have on their shoes?",
-      a: "Buttons",
-      b: "Shoes",
-      c: "Stars",
-      d: "Bells",
-      correct: "d",
-     },
-     {
-      question: "How many reindeer drive Santa's sleigh (including Rudolph)?",
-      a: "Six",
-      b: "Eight",
-      c: "Nine",
-      d: "Ten",
-      correct: "c",
-    },
-  ];
+const quizData = [{
+    question: "Where did the Christmas tree originate from?",
+    a: "Germany",
+    b: "Ireland",
+    c: "Norway",
+    d: "Sweden",
+    correct: "a",
+  },
+  {
+    question: "Who invented electric lights for the Christmas tree?",
+    a: "Ernest Hemingway",
+    b: "Saul Bellow",
+    c: "Edward H. Johnson",
+    d: "Frank Kafka",
+    correct: "c",
+  },
+  {
+    question: "Where is the oldest Christmas market in the world?",
+    a: " Cologne Germany",
+    b: "Dresden Striezelmarkt",
+    c: "Birmingham Christmas Market",
+    d: "Seville's Christmas Market Spain",
+    correct: "b",
+  },
+  {
+    question: "What is the name for the period in between Christmas and New Year?",
+    a: "Turxmas",
+    b: "Landxmas",
+    c: "Twelvetide",
+    d: "Twixmas",
+    correct: "d",
+  },
+  {
+    question: "What is the most popular Christmas tree topper ornament?",
+    a: "An angel",
+    b: "Snowflake",
+    c: "Star",
+    d: "Elf",
+    correct: "a",
+  },
+  {
+    question: "What country started the tradition of hanging stockings?",
+    a: "Canada",
+    b: "Australia",
+    c: "Italy",
+    d: "Germany",
+    correct: "c",
+  },
+  {
+    question: "Which popular christmas beverage is also called 'milk punch?",
+    a: "Milkpunch",
+    b: "Eggnog",
+    c: "Milkbag",
+    d: "Eggpunch",
+    correct: "b",
+  },
+  {
+    question: "What plant is sometimes known as the 'Christmas flower?",
+    a: "Bromeliad",
+    b: "amarillis",
+    c: "Shamrock",
+    d: "Poinsettias",
+    correct: "d",
+  },
+  {
+    question: "In which country is it tradition to eat KFC for Christmas dinner?",
+    a: "Japan",
+    b: "Germany",
+    c: "Ireland",
+    d: "China",
+    correct: "a",
+  },
+  {
+    question: "1.	What is the name of the ballet that takes place at Christmas?",
+    a: "The Sleeping Beauty",
+    b: "The Nutcracker",
+    c: "The Little Match Girl",
+    d: "The Snowman",
+    correct: "b",
+  },
+  {
+    question: "What is traditionally hidden inside a Christmas pudding?",
+    a: "Button",
+    b: "Pin",
+    c: "A coin",
+    d: "Stone",
+    correct: "c",
+  },
+  {
+    question: "If you are born on Christmas day, what is your star sign?",
+    a: "Taurus",
+    b: "Capricorn",
+    c: "Virgo",
+    d: "Aries",
+    correct: "b",
+  },
+  {
+    question: "According to tradition, when should you have your Christmas decorations down by?",
+    a: "January 5th",
+    b: "January 2nd",
+    c: " January 7th",
+    d: "January 14yh",
+    correct: "a",
+  },
+  {
+    question: "What decoration do elves traditionally have on their shoes?",
+    a: "Buttons",
+    b: "Shoes",
+    c: "Stars",
+    d: "Bells",
+    correct: "d",
+  },
+  {
+    question: "How many reindeer drive Santa's sleigh (including Rudolph)?",
+    a: "Six",
+    b: "Eight",
+    c: "Nine",
+    d: "Ten",
+    correct: "c",
+  },
+];
 //Got elements from DOM
-const quiz = document.getElementById("quiz"); 
+const quiz = document.getElementById("quiz");
 const answerQuiz = document.querySelectorAll(".answer");
 const questionQuiz = document.getElementById("question");
 const text_aQuiz = document.getElementById("a_text");
@@ -137,31 +135,31 @@ const submitBtn = document.getElementById("submit");
 //variables
 let currentQuiz = 0;
 let score = 0;
-
+let counter = 20;
 loadQuiz()
 // functions for loading question with answers
 function loadQuiz() {
   deselectAnswers()
   const currentQuizData = quizData[currentQuiz]
-
+  counter = 20;
   questionQuiz.innerText = currentQuizData.question
   text_aQuiz.innerText = currentQuizData.a
   text_bQuiz.innerText = currentQuizData.b
   text_cQuiz.innerText = currentQuizData.c
-  text_dQuiz.innerText= currentQuizData.d
-
+  text_dQuiz.innerText = currentQuizData.d
+  question_Number.innerText =  currentQuiz + 1;
 }
 //function unchecking answers
 function deselectAnswers() {
   answerQuiz.forEach(option => option.checked = false)
 }
-//
+// It return the selected answer
 function getSelected() {
   let answer
   answerQuiz.forEach(answerQuiz => {
-  if(answerQuiz.checked) {
-    answer = answerQuiz.id
-  }
+    if (answerQuiz.checked) {
+      answer = answerQuiz.id
+    }
 
   })
   return answer
@@ -171,54 +169,54 @@ function getSelected() {
 
 //creating submit button for each question
 submitBtn.addEventListener('click', () => {
-const answer = getSelected()
-if(answer) {
-  if (answer === quizData[currentQuiz].correct) {
-    score++
-  } 
-  currentQuiz++
+  const answer = getSelected()
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++
+    }
+    currentQuiz++
 
-    if(currentQuiz < quizData.length) {
-    loadQuiz()
-    
+    if (currentQuiz < quizData.length) {
+      loadQuiz()
+
     } else {
-    quiz.innerHTML =`
+      quiz.innerHTML = `
     <h2>You answered ${score}/${quizData.length} question correctly
     <button onclick ="location.reload()"
-    ` 
+    `
     }
   }
 })
 
 // will change question numberers from1-15
 //Creating Timer For Quiz Timer Section
-let counter = 20;
+
 let questionCount = 0;
 let questions = quizData[currentQuiz];
-questionDivID = document.getElementById("questionNumber");
-  
-setInterval(function() {
+// questionDivID = document.getElementById("questionNumber");
+
+setInterval(function () {
   counter--;
-   if (counter >= 0) {
-    id = document.getElementById("timer");
-    id.innerHTML = counter;
-    questionCount++;
-   }
-   if(counter === 0 ) {
-    id.innerHTML = "Time Up!";
-    counter = 20;
-    questionCount++;
-   }
-   if (questionCount === currentQuiz.length) {
-    questionDivID.innerHTML = "Well Played! Game is over";
-    id.innerHTML = "";
-   } else { questionDivID.innerHTML = currentQuiz;
-   }
-},1000);
+  if (counter >= 0) {
+    questionTimer.innerText = counter;
+  }
+  if (counter === 0) {
+    questionTimer.innerText = "Time Up!";
+    currentQuiz++
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz()
+
+    } else {
+      quiz.innerHTML = `
+    <h2>You answered ${score}/${quizData.length} question correctly
+    <button onclick ="location.reload()"
+    `
+    }
+  }
+}, 1000);
+
 function goToNextQuestion() {
   questionCount++;
   counter = 20;
 }
-
-
- 
